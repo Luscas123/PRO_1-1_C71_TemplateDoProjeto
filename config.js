b097c378c740f1f9594aaf9c3939e390684e12c2
Luscas123 <102263815+Luscas123@users.noreply.github.com>
2023-03-22 19:49:50 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,16 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 var firebaseConfig = {
-  //Cole sua Configuração do Firebase aqui
+  const firebaseConfig = {
+  apiKey: "AIzaSyA8OHpEdHkKZaXMRIxo-lwh08O93nEsq5k",
+  authDomain: "ciclista-eletroni.firebaseapp.com",
+  databaseURL: "https://ciclista-eletroni-default-rtdb.firebaseio.com",
+  projectId: "ciclista-eletroni",
+  storageBucket: "ciclista-eletroni.appspot.com",
+  messagingSenderId: "537585784524",
+  appId: "1:537585784524:web:ab8e54fec65a4275c8d469"
+};
+
   
 };
 
